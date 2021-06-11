5c2503efe948652da054c6aee1d38fc86814c11a
sean <sean@sean>
2021-06-10 21:45:45 -0500

    Added new variable pokemonList + array of 3 objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood = 'vegetable pho';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: "Blastoise", height: 5, types: ["Water"]},
+  {name: "Metapod", height: 2, types: ["Bug"]},
+  {name: "Venusaur", height: 6, types:["Grass","Poison"]},
+];  
+
